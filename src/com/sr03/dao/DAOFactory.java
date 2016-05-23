b65a5c8873e68fcaafe4941dcdf689ee6fb18cd9
RapaelKovacic <raphael.kovacic@gmail.com>
2016-05-23 23:58:03 +0200

    quizz

--- src/com/sr03/dao/DAOFactory.java
+++ src/com/sr03/dao/DAOFactory.java
@@ -76,4 +76,8 @@ public class DAOFactory {
     public UtilisateurDao getUtilisateurDao() {
         return new UtilisateurDaoImpl( this );
     }
+    
+    public QuizzDAO getQuizzDao() {
+        return new QuizzDaoImpl( this );
+    }
 }
---
+++ src/com/sr03/dao/QuizzDAO.java
@@ -0,0 +1,19 @@
+package com.sr03.dao;
+
+import java.util.List;
+
+import com.sr03.beans.Quizz;
+
+public interface QuizzDAO {
+
+
+    void creer( Quizz quizz ) throws DAOException;
+
+    Quizz trouver( String id ) throws DAOException;
+
+	List<Quizz> viewAllQuizz(int offset, int noOfRecords);
+	
+	int getNoOfRecords();
+
+
+}
